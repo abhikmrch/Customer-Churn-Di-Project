f016ea755d324db0e1742d338fd98b426f2ad881
ABHISHEK <102384822+abhikmrch@users.noreply.github.com>
2025-10-28 01:43:57 +0000

    models

--- train.py
+++ train.py
@@ -17,6 +17,18 @@ X['Gender']= LabelEncoder().fit_transform(X['Gender'])
 
 X_train, x_test, y_train, y_test = train_test_split(X,Y, test_size=0.2,random_state=42)
 #Transform the data
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
+#scaler = StandardScaler()
+#X_train = scaler.fit_transform(X_train)
 
+model = Sequential[(    
+    Dense(16, activation ='relu', input_shape=(X_train.shape[1],)),
+    Dense(8, activation ='relu'),
+    Dense(1, activation = 'sigmoid')
+])
+
+model.compile(optimizer='adam',loss = 'binary_crossentropy', metrics=['accuracy'])
+model.fit(X_train,y_train, epochs=30, batch_size=13, validation_split=0.2,verbose=1)
+
+model.save('models/churn_model.h5')
+
+pd.to_Pickle(Scaler,'.model/scaler.pkl')
